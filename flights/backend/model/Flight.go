c3aaea8f3272bb157e946e198328710370b1f322
lazicv <zumjzk92@gmail.com>
2023-04-02 02:53:10 +0200

    Front create

--- flights/backend/model/Flight.go
+++ flights/backend/model/Flight.go
@@ -3,12 +3,14 @@ package model
 import (
 	"time"
 
+	//"github.com/google/uuid"
+	//"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Flight struct {
 	Id primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	//ID            uuid.UUID          `bson:"id" json:"id"`
+	//ID             uuid.UUID `bson:"id" json:"id"`
 	Departure      time.Time `bson:"departure" json:"departure"`
 	Origin         Location  `bson:"origin" json:"origin"`
 	Destination    Location  `bson:"destination" json:"destination"`
@@ -17,6 +19,6 @@ type Flight struct {
 }
 
 /*func (flight *Flight) BeforeCreate(*mongo.Database) error {
-	flight.ID1 = uuid.New()
+	flight.ID = uuid.New()
 	return nil
 }*/
