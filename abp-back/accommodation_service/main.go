be4d51b34b54ae227a350e9f6ef63d4ce9221c59
s-matke <stefan.matkovic@hotmail.com>
2023-05-13 14:00:12 +0200

    main.go

--- abp-back/accommodation_service/main.go
+++ abp-back/accommodation_service/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"github.com/s-matke/abp/abp-back/accommodation_service/startup"
+	cfg "github.com/s-matke/abp/abp-back/accommodation_service/startup/config"
+)
+
 func main() {
+	config := cfg.NewConfig()
+	server := startup.NewServer(config)
+	server.Start()
 
 }
