6d7db543c8ca6a9e60e28f4ae04979208f0c7219
s-matke <stefan.matkovic@hotmail.com>
2023-05-14 10:53:09 +0200

    create pricing proton

--- abp-back/common/proto/pricing_service/pricing_service.pb.gw.go
+++ abp-back/common/proto/pricing_service/pricing_service.pb.gw.go
@@ -153,6 +153,40 @@ func local_request_PricingService_GetAll_0(ctx context.Context, marshaler runtim
 
 }
 
+func request_PricingService_CreatePricing_0(ctx context.Context, marshaler runtime.Marshaler, client PricingServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq CreatePricingRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq.Pricing); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.CreatePricing(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_PricingService_CreatePricing_0(ctx context.Context, marshaler runtime.Marshaler, server PricingServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq CreatePricingRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq.Pricing); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.CreatePricing(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterPricingServiceHandlerServer registers the http handlers for service PricingService to "mux".
 // UnaryRPC     :call PricingServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -231,6 +265,31 @@ func RegisterPricingServiceHandlerServer(ctx context.Context, mux *runtime.Serve
 		}
 
 		forward_PricingService_GetAll_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("POST", pattern_PricingService_CreatePricing_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/pricing.PricingService/CreatePricing", runtime.WithHTTPPathPattern("/pricing"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_PricingService_CreatePricing_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_PricingService_CreatePricing_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -341,6 +400,28 @@ func RegisterPricingServiceHandlerClient(ctx context.Context, mux *runtime.Serve
 
 	})
 
+	mux.Handle("POST", pattern_PricingService_CreatePricing_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/pricing.PricingService/CreatePricing", runtime.WithHTTPPathPattern("/pricing"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_PricingService_CreatePricing_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_PricingService_CreatePricing_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -350,6 +431,8 @@ var (
 	pattern_PricingService_GetByAccommodation_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1}, []string{"pricing", "accommodation_id"}, ""))
 
 	pattern_PricingService_GetAll_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"pricing"}, ""))
+
+	pattern_PricingService_CreatePricing_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"pricing"}, ""))
 )
 
 var (
@@ -358,4 +441,6 @@ var (
 	forward_PricingService_GetByAccommodation_0 = runtime.ForwardResponseMessage
 
 	forward_PricingService_GetAll_0 = runtime.ForwardResponseMessage
+
+	forward_PricingService_CreatePricing_0 = runtime.ForwardResponseMessage
 )
