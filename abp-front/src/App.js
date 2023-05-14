994abd9c2ba16e613c7c2acf10592d2e1ec35c24
Stefan <skstefankalicanin@gmail.com>
2023-05-14 16:39:08 +0200

    pricing handle

--- abp-front/src/App.js
+++ abp-front/src/App.js
@@ -6,7 +6,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import Sidebar from "./components/Sidebar/Sidebar";
 //import Register from './Pages/Register';
 import CreateAccommodation from './Pages/CreateAccommodation';
-
+import ShowAccommodation from './Pages/ShowAccommodation';
 function App() {
   return (
     <>
@@ -16,6 +16,7 @@ function App() {
           {/*<Route path="/signup" element={<Register />}></Route> */}
           {/* <Route path="/" element={<Signin />}></Route> */}
           <Route path="/createAccommodation" element={<CreateAccommodation/>}></Route>
+          <Route path="/accommodation" element={<ShowAccommodation/>}></Route>
         </Routes>
       </BrowserRouter>
       <ToastContainer/>
